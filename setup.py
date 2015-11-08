9c9622978cd07f08a9ac63b718c60d0b63aa5cc8
Verdan Mahmood <verdan.mahmood@gmail.com>
2015-11-09 01:43:24 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class import_cldr(Command):
 
 setup(
     name='Babel',
-    version='3.0-dev',
+    version='3.0',
     description='Internationalization utilities',
     long_description=\
 """A collection of tools for internationalizing Python applications.""",
